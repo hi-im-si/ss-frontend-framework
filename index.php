9369a059bf43adcb48fb5ee37354d8f70f8f1f28
Simon Shahriveri <si@hi-im-si.com>
2014-10-06 16:32:02 +0100

    [all] house keeping

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
+
+		<?/* Prefetch DNS for external assets (Twitter widgets, Maps etc). */?>
+		<link rel="dns-prefetch" href="//ajax.googleapis.com">
+		<link rel="dns-prefetch" href="//www.google-analytics.com">
+
+		<?/* Prerender pages for browser preloading */?>
+		<?/*<link rel="prerender" href="/link/to/another/page" />*/?>
+
+
 		<title></title>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<meta name="revisit-after" content="14 days" />
@@ -15,30 +23,36 @@
 		<meta name="author" content="" />
 		<link rel="canonical" href="" />
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
 		<link rel="shortcut icon" href="/icon.png">
 		<link rel="apple-touch-icon-precomposed" href="/icon.png">
 
-		<link rel="stylesheet" href="/assets/css/main.min.css">
+		<link rel="stylesheet" href="/assets/css/main.css">
 
 		<script src="/assets/js/vendor/modernizr-2.7.1.js"></script>
-		<script src="/assets/js/vendor/conditionizr.min.js"></script>
 	</head>
-	<body id="home-page" data-page="home">
+
+
+	<body id="home-page" data-page="home" itemscope itemtype="http://schema.org/WebPage">
 
 		<div class="page  page--home">
 
-			<h1>Kickstart this Mo'Fo</h1>
+			<main role="main">
 
-		</div>
+				<h1>Kickstart this Mo'Fo</h1>
+
+	        </main>
+
+		</div> <!-- /page -->
 
 
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
-		<!--
+		<script src="/assets/js/vendor/conditionizr.min.js"></script>
+
 		<script src="/assets/js/lib/imager.js"></script>
 		<script src="/assets/js/lib/magnific-popup.js"></script>
 
@@ -51,9 +65,7 @@
 		<script src="/assets/js/module/conditionizr-init.js"></script>
 
 		<script src="/assets/js/src/main.js"></script>
-		-->
-
-		<script src="/assets/js/main.min.js"></script>
 
 	</body>
+
 </html>

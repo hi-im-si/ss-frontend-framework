6e7ca78c98fb3960ad2fc306de36f2572c837e52
Simon Shahriveri <si@hi-im-si.com>
2014-11-10 23:19:54 +0000

    [js] forms.js now following consistent setup to allow for coherent and readable structure

--- assets/js/objects/forms.js
+++ assets/js/objects/forms.js
@@ -1,7 +1,24 @@
+FORMS = (function ($) {
 
-	function createSelects() {
+	init = function () {
+		// Code here runs straight away
 
-		$('select.select').each(function(){
+		$(DOMready);
+
+	},
+
+	DOMready = function () {
+		// Code here runs when the DOM is ready
+		$('form').placeholder();
+		$('form').radioWrapper();
+		createSelects('select.select');
+		createCheckbox();
+	};
+
+	createSelects = function(elem) {
+
+		$(elem).each(function(){
+
 			if (!$(this).parent().is('.select-wrap')) {
 				/* added select-wrap around select and span as it cant be done with php */
 				$(this).wrap("<div class='select-wrap' />");
@@ -22,8 +39,8 @@
 				});
 		});
 
-		$('select').each(function () {
-			if($(this).hasClass('error')) {
+		$(elem).each(function () {
+			if($(this).parents().hasClass('error')) {
 				$(this).siblings('span.select').addClass('error-select');
 			}
 		});
@@ -31,7 +48,7 @@
 	}
 
 
-	function createCheckbox() {
+	createCheckbox = function() {
 
 		$('input:checkbox').wrap('<span class="checkbox"></span>');
 		$('input:checkbox:checked').parent('span.checkbox').addClass('checkbox--selected');
@@ -51,35 +68,10 @@
 	}
 
 
-	jQuery(function($) {
+	return {
+		start : init
+	};
 
+})(jQuery);
 
-		/*======================================================================================================
-		========================== Initiate Placeholder Fallback ===============================================
-		========================================================================================================*/
-
-		$('form').placeholder();
-
-
-		/*======================================================================================================
-		========================== Initiate Radio Styling ======================================================
-		========================================================================================================*/
-
-		$('form').radioWrapper();
-
-		/*======================================================================================================
-		========================== Initiate Select Styling =====================================================
-		========================================================================================================*/
-
-		createSelects();
-
-
-		/*======================================================================================================
-		========================== Checkbox Styling ============================================================
-		========================================================================================================*/
-
-		createCheckbox();
-
-
-
-	}); // end doc ready
+FORMS.start();

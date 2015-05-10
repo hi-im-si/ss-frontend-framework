fbf914fbeab74061dd428e12f0c05f13c1f4e805
Simon Shahriveri <si@hi-im-si.com>
2015-05-10 19:26:53 +0100

    [js] added scroll/resize/imager funcs as standard to main.js

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -9,6 +9,30 @@ MAIN = (function ($) {
 	DOMready = function () {
 		// Code here runs when the DOM is ready
 
+		Scroll();
+		Resize();
+
+		imager('.js-load-img');
+
+	},
+
+	Scroll = function() {
+		$(window).on('scroll', function (e) {
+
+		});
+	},
+
+	Resize = function() {
+		$(window).on('resize', function (e) {
+
+		});
+	},
+
+	imager = function(el) {
+		new Imager(el, {
+			//availableWidths: [360, 480, 600, 800, 1024],
+			lazyload: true
+		});
 	};
 
 	return {

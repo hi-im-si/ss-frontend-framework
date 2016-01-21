c4929d304f7992578b1691fb3c3ba7b99634a1cd
Simon Shahriveri <si@hi-im-si.com>
2016-01-21 17:57:52 +0000

    [js/forms] rename file as gulp compiles in wrong order

--- assets/js/objects/zforms.js
+++ assets/js/objects/zforms.js
@@ -9,19 +9,18 @@ FORMS = (function ($) {
 
 	DOMready = function () {
 		// Code here runs when the DOM is ready
-		$('form').placeholder();
 		$('form').radioWrapper();
-		createSelects('select.select');
-		createCheckbox();
+		createSelects('.js-select');
+		createCheckbox('checkbox', 'checkbox--selected');
 	};
 
 	createSelects = function(elem) {
 
 		$(elem).each(function(){
 
-			if (!$(this).parent().is('.select-wrap')) {
+			if (!$(this).parent().is('.selectdd')) {
 				/* added select-wrap around select and span as it cant be done with php */
-				$(this).wrap("<div class='select-wrap' />");
+				$(this).wrap("<div class='selectdd' />");
 			}
 
 			var title = $(this).attr('title');
@@ -29,8 +28,8 @@ FORMS = (function ($) {
 			if($('option:selected', this).val()) { title = $('option:selected',this).text() };
 
 			$(this)
-				.css({'z-index':10,'opacity':0})
-				.after('<span class="select">' + title + '</span>')
+				.css({ 'opacity': 0 })
+				.after('<span class="selectdd__option">' + title + '</span>')
 				.on('change, change.address-form', function(){
 					var val = $(this).find(':selected').text().length > 0 ? $(this).find(':selected').text() : 'Select';
 					// TODO find a way to do this without repeating the markup.
@@ -41,29 +40,32 @@ FORMS = (function ($) {
 
 		$(elem).each(function () {
 			if($(this).parents().hasClass('error')) {
-				$(this).siblings('span.select').addClass('error-select');
+				$(this).siblings('.selectdd__option').addClass('selectdd__option--error');
 			}
 		});
 
 	}
 
 
-	createCheckbox = function() {
+	createCheckbox = function(elem, selected) {
 
-		$('input:checkbox').wrap('<span class="checkbox"></span>');
-		$('input:checkbox:checked').parent('span.checkbox').addClass('checkbox--selected');
+		$('input:checkbox').wrap('<span class="'+elem+'"></span>');
+		$('input:checkbox:checked').parent('span.'+elem).addClass(selected);
 
-		$('span.checkbox').click(function (e) {
-			if($(this).hasClass('checkbox--selected')) {
-				$(this).removeClass('checkbox--selected');
-				$(this).children('input').removeAttr("checked");
+		$('span.'+elem).on('click', function (e) {
+
+			var $this = $(this);
+
+			if( $this.hasClass(selected) ) {
+				$this.removeClass(selected);
+				$this.children('input').removeAttr("checked");
 			} else {
-				$(this).addClass('checkbox--selected');
-				$(this).children('input').attr("checked", true);
+				$this.addClass(selected);
+				$this.children('input').attr("checked", true);
 			}
 
 			// fire change event
-			$(this).children('input:checkbox:not(#shipping_chk)').change();
+			$this.children('input:checkbox:not(#shipping_chk)').change();
 		});
 	}
 

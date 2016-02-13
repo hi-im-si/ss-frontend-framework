b22168224a9a624e6fb23dcaa4dc9b3deb2d61b4
Simon Shahriveri <si@hi-im-si.com>
2016-02-13 23:36:03 +0000

    [template] split template into ncludes

---
+++ inc/head.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+
+		<?/* Prefetch DNS for external assets (Twitter widgets, Maps etc). */?>
+		<link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
+    	<link rel="dns-prefetch" href="//www.google-analytics.com">
+
+		<?/* Prerender pages for browser preloading */?>
+		<?/*<link rel="prerender" href="/link/to/another/page" />*/?>
+
+
+		<title></title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="revisit-after" content="14 days" />
+		<meta name="robots" content="all" />
+		<meta name="generator" content="" />
+		<meta name="author" content="" />
+		<link rel="canonical" href="" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+		<script src="https://use.typekit.net/igu4pcb.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+		<!-- // <script src="https://file.myfontastic.com/xpt7B33jagNPhrWhAKTqgg/icons.js"></script> -->
+		<link href="https://file.myfontastic.com/xpt7B33jagNPhrWhAKTqgg/icons.css" rel="stylesheet">
+
+		<link rel="stylesheet" href="assets/css/main.css">
+
+		<? include 'modernizr.php'; ?>
+
+		<script>
+		Modernizr.load([{load:"timeout=500!//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js",complete:function(){Modernizr.load(window.jQuery?[{load:'assets/js/main.min.js',complete:function(){window.init()}}]:[{load:"timeout=120000!assets/js/vendor/jquery-1.11.3.min.js",complete:function(){Modernizr.load([{load:'assets/js/main.min.js',complete:function(){window.init()}}])}}])}}]);
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-XXXXXXX-X','auto');
+		</script>
+
+	</head>
+
+
+	<body id="home-page" data-page="home" itemscope itemtype="http://schema.org/WebPage">

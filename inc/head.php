91d5367a676f1c29c9eb6d293fe248b909b09765
Simon Shahriveri <si@hi-im-si.com>
2017-02-01 17:50:21 +0000

    [template] remove script calls from head and add GA back

--- inc/head.php
+++ inc/head.php
@@ -26,16 +26,14 @@
 
 		<link rel="stylesheet" href="assets/css/main.css">
 
+
 		<script>
 		<?= file_get_contents("assets/js/vendor/modernizr.min.js"); ?>
 
 		<?= file_get_contents("assets/js/vendor/lazyload.min.js"); ?>
 
-		LazyLoad.js(['//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js','/kickstarter/assets/js/main.min.js'],function(){window.init()});
-
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-XXXXXXX-X','auto');
 		</script>
-
 	</head>
 
 

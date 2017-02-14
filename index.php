b14786e21ac2e70ec56db1eb8e069e06fc9284d1
Simon Shahriveri <si@hi-im-si.com>
2017-02-14 08:56:03 +0000

    [index] spacing

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 	<div class="page  page--home">
 
+
 		<main class="l-wrap  l-main" role="main">
 
 			<h1>Welcome to SS Front-end Framework</h1>
 
 		</main> <!-- /l-main -->
+
 
 	</div> <!-- /page -->
 
